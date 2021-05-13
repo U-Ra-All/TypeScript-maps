311243679369396df6900091533962e1db7b36a5
YouRa <allakhverdovyuriy@gmail.com>
2021-05-13 10:18:51 +0300

    Add TDF

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
+/// <reference types="@types/googlemaps" />
 import {User} from './User';
 import {Company} from './Company';
+
 
 const user = new User();
 console.log(user);
 
 const company = new Company();
 console.log(company);
+
+google
