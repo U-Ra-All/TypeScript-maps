341b148c0ece9ab170aa797fb5354a9bf17b0b80
YouRa <allakhverdovyuriy@gmail.com>
2021-05-13 09:07:02 +0300

    Add Google API key

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -136974,7 +136974,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63249" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62186" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
